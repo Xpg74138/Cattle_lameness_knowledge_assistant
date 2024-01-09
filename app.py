b3a46592f5f6dc99f2fd9b834d60e4884290f553
Xpg74138 <50387027+Xpg74138@users.noreply.github.com>
2024-01-09 21:07:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 # 设置环境变量
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # 下载模型
 os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir ./sentence-transformer')
 
